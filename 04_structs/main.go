9efcd7b974f060f186a04cb3aaccdf0748ac0089
hidalz <46967257+hidalz@users.noreply.github.com>
2024-09-16 08:34:24 +0200

    feat: 04-04 Nested structs

--- 04_structs/main.go
+++ 04_structs/main.go
@@ -2,9 +2,33 @@ package main
 
 // 1. Structs in Go
 
-type messageToSend struct {
-	phoneNumber int
-	message string	
-}
+// type messageToSend struct {
+// 	phoneNumber int
+// 	message string
+// }
 
 // 2. Nested structs in Go
+type messageToSend struct {
+	message   string
+	sender    user
+	recipient user
+}
+
+type user struct {
+	name   string
+	number int
+}
+
+func canSendMessage(mToSend messageToSend) bool {
+	// ?
+	if mToSend.sender.number == 0 || mToSend.recipient.number == 0 {
+		return false
+	}
+
+	if mToSend.sender.name == "" || mToSend.recipient.name == "" {
+		return false
+	}
+	return true
+}
+
+// 3. Embedded structs
