dec25847650b532ef0c9ec13c9a3ec82013c664f
hidalz <46967257+hidalz@users.noreply.github.com>
2024-10-01 08:33:55 +0200

    feat: advance tutorial

--- go_by_example/closures/closures.go
+++ go_by_example/closures/closures.go
@@ -1,1 +1,23 @@
 package main
+
+import "fmt"
+
+// intSeq returns an anonymous function that is a closure
+func intSeq() func() int {
+	i := 0              // Variable in the outer function's scope
+	return func() int { // Anonymous function
+		i++ // The anonymous function captures and modifies 'i'
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSeq() // nextInt is a closure that captures 'i'
+
+	fmt.Println(nextInt()) // Prints 1
+	fmt.Println(nextInt()) // Prints 2
+	fmt.Println(nextInt()) // Prints 3
+
+	newInts := intSeq()    // newInts is a new closure with a new 'i'
+	fmt.Println(newInts()) // Prints 1
+}
--- go_by_example/variadic-functions/variadic-functions.go
+++ go_by_example/variadic-functions/variadic-functions.go
@@ -19,4 +19,15 @@ func main() {
 
 	nums := []int{1, 2, 3, 4}
 	sum(nums...)
+
+	// Convert []int to []any
+	anyNums := make([]any, len(nums))
+	for i, v := range nums {
+		anyNums[i] = v
+	}
+
+	fmt.Println(anyNums...) // Here, I can't put "string", value since it would be too many values. Would need to include it
+
+	anynumsdirect := []any{1, 2, 3, 4}
+	fmt.Println("anynumsdirect: ", anynumsdirect)
 }
