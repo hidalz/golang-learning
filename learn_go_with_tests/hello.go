f82b14b91dd53a3be86e632e76dc4b77a756a4bf
hidalz <46967257+hidalz@users.noreply.github.com>
2024-10-16 18:12:31 +0200

    feat: advance tutorial

--- learn_go_with_tests/hello.go
+++ learn_go_with_tests/hello.go
@@ -2,15 +2,37 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	italian            = "Italian"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+	italianHelloPrefix = "Bongiorno, "
+)
 
-func Hello(name string) string {
+func Hello(name string, language string) string { // Public function (capital letter)
 	if name == "" {
-		name = "Wrld"
+		name = "World"
 	}
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) { // Private functions (lowercase letter)
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case italian:
+		prefix = italianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
