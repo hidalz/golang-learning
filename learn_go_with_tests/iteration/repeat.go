5180cded6d51f4ceac7a2bf6f3fd94c529aaa829
hidalz <46967257+hidalz@users.noreply.github.com>
2024-10-30 17:19:55 +0100

    feat: advance tutorial - iteration

--- learn_go_with_tests/iteration/repeat.go
+++ learn_go_with_tests/iteration/repeat.go
@@ -11,3 +11,5 @@ func Repeat(character string, number int) string {
 func ConcatenateFoo(word string) string {
 	return word + "foo"
 }
+
+//TODO: Have a look through the strings package. Find functions you think could be useful and experiment with them by writing tests like we have here. Investing time learning the standard library will really pay off over time.
