21b6a8a86921948cffbd8005daa5383925a083b3
hidalz <46967257+hidalz@users.noreply.github.com>
2024-10-31 08:55:02 +0100

    feat: advance tutorial - arrays_and_slices

--- learn_go_with_tests/arrays_and_slices/sum.go
+++ learn_go_with_tests/arrays_and_slices/sum.go
@@ -13,13 +13,15 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-func SumAll(numbersToSum ...[]int) []int {
-	numberLength := len(numbersToSum)
-
-	sums := make([]int, numberLength)
-
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
 	return sums
