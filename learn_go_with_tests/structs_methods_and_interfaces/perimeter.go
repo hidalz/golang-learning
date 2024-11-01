9381a627d1bafaa46153ab2af085916aa47d6913
hidalz <46967257+hidalz@users.noreply.github.com>
2024-11-01 19:26:17 +0100

    feat: advance tutorial - structs, methods & interfaces

--- learn_go_with_tests/structs_methods_and_interfaces/perimeter.go
+++ learn_go_with_tests/structs_methods_and_interfaces/perimeter.go
@@ -23,6 +23,19 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-// func (c Circle) Perimeter() float64 {
-// 	return 2 * math.Pi * c.Radius
-// }
+//	func (c Circle) Perimeter() float64 {
+//		return 2 * math.Pi * c.Radius
+//	}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
+}
+
+type Shape interface {
+	Area() float64
+}
