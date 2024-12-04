76791b871e1b75ed8a395d5ee2c1521ff638135e
hidalz <46967257+hidalz@users.noreply.github.com>
2024-12-04 09:15:04 +0100

    feat: advance tutorial - managing files

---
+++ learn_go_with_tests/blogposts/go.mod
@@ -0,0 +1,5 @@
+module github.com/hidalz/blogposts
+
+go 1.23.3
+
+require github.com/quii/fstest-spike v0.0.0-20210603161226-22f21fe78577
---
+++ learn_go_with_tests/blogposts/post.go
@@ -0,0 +1,49 @@
+package blogposts
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type Post struct {
+	Title       string
+	Description string
+	Tags        []string
+	Body        string
+}
+
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+	tagsSeparator        = "Tags: "
+)
+
+// newPost creates a new Post from a file
+func newPost(postFile io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postFile)
+
+	readMetaLine := func(tagName string) string {
+		scanner.Scan()
+		return strings.TrimPrefix(scanner.Text(), tagName)
+	}
+
+	return Post{
+		Title:       readMetaLine(titleSeparator),
+		Description: readMetaLine(descriptionSeparator),
+		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
+		Body:        readBody(scanner),
+	}, nil
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan() // ignore a line
+
+	buf := bytes.Buffer{}
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+	return strings.TrimSuffix(buf.String(), "\n")
+}
