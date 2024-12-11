fa7c071613043e9e485552debbc23833601e97cf
hidalz <46967257+hidalz@users.noreply.github.com>
2024-12-11 08:55:48 +0100

    feat: advance tutorial - templating

--- learn-go-with-tests/blogrenderer/blogrender.go
+++ learn-go-with-tests/blogrenderer/blogrender.go
@@ -5,6 +5,9 @@ import (
 	"embed"
 	"html/template"
 	"io"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/parser"
 )
 
 var (
@@ -12,16 +15,38 @@ var (
 	postTemplates embed.FS
 )
 
-func Render(w io.Writer, p blogposts.Post) error {
+type PostRenderer struct {
+	templ    *template.Template
+	mdParser *parser.Parser
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
 	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
-
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	if err := templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
-		return err
-	}
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+	parser := parser.NewWithExtensions(extensions)
 
-	return nil
+	return &PostRenderer{templ: templ, mdParser: parser}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p blogposts.Post) error {
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", newPostVM(p, r))
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []blogposts.Post) error {
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
+
+type postViewModel struct {
+	blogposts.Post
+	HTMLBody template.HTML
+}
+
+func newPostVM(p blogposts.Post, r *PostRenderer) postViewModel {
+	vm := postViewModel{Post: p}
+	vm.HTMLBody = template.HTML(markdown.ToHTML([]byte(p.Body), r.mdParser, nil))
+	return vm
 }
--- learn-go-with-tests/blogrenderer/go.mod
+++ learn-go-with-tests/blogrenderer/go.mod
@@ -7,4 +7,5 @@ replace blogposts => ../blogposts
 require (
 	blogposts v0.0.0-00010101000000-000000000000
 	github.com/approvals/go-approval-tests v0.0.0-20241205182534-1cf6af14f2bb
+	github.com/gomarkdown/markdown v0.0.0-20241205020045-f7e15b2f3e62
 )
